98d0d116ae70c711d97e916c4753a583c80b2a10
Patrick Sinco <psinco@gmail.com>
2014-07-27 17:39:46 -0500

    begins playing around with Route

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,50 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	//return View::make('hello');
+  return 'Hello, world';
+});
+
+Route::get('/books', function() {
+  return 'Here are all the books ... ';
+});
+
+Route::get('/books/{category}', function($category) {
+  return "here are all the books in $category";
+});
+
+Route::get('/new', function() {
+
+  $view = "<form method='POST'>";
+  $view .= "Title: <input type='text' name='title'>";
+  $view .= "<input type='submit'>";
+  $view .= "</form>";
+  return $view;
+
+});
+
+Route::post('/new', function() {
+  //$input = Input::get('title');
+  $input = Input::all();
+  print_r($input);
+});
+
+Route::get('/practice', function() {
+  echo 'Hello, world' . "\r\n";
+  echo App::environment();
+});
+
+
+Route::get('/practice/pre', function() {
+
+  $simpsons = array('Homer', 'Marge', 'Lisa',
+    'Bart', 'Maggie');
+  $brady_boys = array('Mike', 'Greg', 'Peter', 'Bobby');
+
+  // note: 'Paste\Pre' is aliased in /app/config/app.php
+  Pre::add($simpsons, 'debug 1');
+  Pre::add($brady_boys, 'debug 2');
+
+  echo Paste\Pre::r();
+
 });
